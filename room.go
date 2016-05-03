0853e5530a9594d1ea9993a8693b60558e03fac0
Ryu Hanaoka <doradora56@gmail.com>
2016-05-04 03:35:41 +0900

    trace型のメッセージを追加

--- room.go
+++ room.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"github.com/dora56/chat/trace"
 	"github.com/gorilla/websocket"
 	"log"
 	"net/http"
@@ -15,6 +16,8 @@ type room struct {
 	leave chan *client
 	// clientsには在籍しているすべてのクライアントが保持されます。
 	clients map[*client]bool
+	// tracerはチャットルーム上で行われた操作のログを受け取ります。
+	tracer trace.Tracer
 }
 
 // newRoomはすぐに利用できるチャットルームを生成できます。
@@ -33,20 +36,25 @@ func (r *room) run() {
 		case client := <-r.join:
 			//参加
 			r.clients[client] = true
+			r.tracer.Trace("新しい新しいクライアントが参加しました")
 		case client := <-r.leave:
 			//退出
 			delete(r.clients, client)
 			close(client.send)
+			r.tracer.Trace(" クライアントが退出しました")
 		case msg := <-r.forward:
+			r.tracer.Trace("メッセージを受信しました:", string(msg))
 			//すべてのクライアントにメッセージを転送
 			for client := range r.clients {
 				select {
 				case client.send <- msg:
 					// メッセージを送信
+					r.tracer.Trace(" -- クライアントに送信されました")
 				default:
 					// 送信に失敗
 					delete(r.clients, client)
 					close(client.send)
+					r.tracer.Trace(" -- s送信に失敗しました。クライアントをクリーンアップします")
 				}
 			}
 		}
