573376116e8c12bed6eea95024f8c28a6385921a
Ryu Hanaoka <doradora56@gmail.com>
2016-05-06 03:43:18 +0900

    2.6.4 自分のユーザー名の表示 p53

--- room.go
+++ room.go
@@ -3,13 +3,14 @@ package main
 import (
 	"github.com/dora56/chat/trace"
 	"github.com/gorilla/websocket"
+	"github.com/stretchr/objx"
 	"log"
 	"net/http"
 )
 
 type room struct {
 	// forwardは他のクライアントに転送するためのメッセージを保持するチャネルです。
-	forward chan []byte
+	forward chan *message
 	// joinはチャットルームに参加しようとしているクライアントのためのチャネルです。
 	join chan *client
 	// leaveはチャットルームを退出しようとしているクライアントのためのチャネルです。
@@ -23,7 +24,7 @@ type room struct {
 // newRoomはすぐに利用できるチャットルームを生成できます。
 func newRoom() *room {
 	return &room{
-		forward: make(chan []byte),
+		forward: make(chan *message),
 		join:    make(chan *client),
 		leave:   make(chan *client),
 		clients: make(map[*client]bool),
@@ -44,7 +45,7 @@ func (r *room) run() {
 			close(client.send)
 			r.tracer.Trace(" クライアントが退出しました")
 		case msg := <-r.forward:
-			r.tracer.Trace("メッセージを受信しました:", string(msg))
+			r.tracer.Trace("メッセージを受信しました:", msg.Message)
 			//すべてのクライアントにメッセージを転送
 			for client := range r.clients {
 				select {
@@ -75,10 +76,17 @@ func (r *room) ServeHTTP(w http.ResponseWriter, req *http.Request) {
 		log.Fatal("ServeHTTP:", err)
 		return
 	}
+
+	authCookie, err := req.Cookie("auth")
+	if err != nil {
+		log.Fatal("クッキーの取得に失敗しました:", err)
+	}
+
 	client := &client{
-		socket: socket,
-		send:   make(chan []byte, messageBufferSize),
-		room:   r,
+		socket:   socket,
+		send:     make(chan *message, messageBufferSize),
+		room:     r,
+		userData: objx.MustFromBase64(authCookie.Value),
 	}
 	r.join <- client
 	defer func() { r.leave <- client }()
