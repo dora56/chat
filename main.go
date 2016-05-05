7f250274088882af8ea2918445b4deb059578265
Ryu Hanaoka <doradora56@gmail.com>
2016-05-05 22:46:59 +0900

    2.6 外部アカウントでのログインの実装 p44

--- main.go
+++ main.go
@@ -5,6 +5,10 @@ import (
 	"log"
 	"net/http"
 	"path/filepath"
+	"src/github.com/stretchr/gomniauth"
+	"src/github.com/stretchr/gomniauth/providers/facebook"
+	"src/github.com/stretchr/gomniauth/providers/github"
+	"src/github.com/stretchr/gomniauth/providers/google"
 	"sync"
 	"text/template"
 )
@@ -27,6 +31,14 @@ func (t *templateHandler) ServeHTTP(w http.ResponseWriter, r *http.Request) {
 func main() {
 	var addr = flag.String("addr", ":8080", "アプリケーションのアドレス")
 	flag.Parse() // フラグを解釈します。
+	//Gomniauthのセットアップ
+	gomniauth.SetSecurityKey("zMsQ7B7FQneKKXTQKviECsjHjvA%RNKA")
+	gomniauth.WithProviders(
+		facebook.New("クライアントID", "秘密の鍵", "http://localhost:8080/auth/callback/facebook"),
+		github.New("クライアントID", "秘密の鍵", "http://localhost:8080/auth/callback/github"),
+		google.New("1028664644457-5gr0979l96j6ku8a2lfrm3id2ph5dvo0.apps.googleusercontent.com",
+			"JXCqNsv0aWeh6p9jv81lCpcx", "http://localhost:8080/auth/callback/google"),
+	)
 	r := newRoom()
 	//r.tracer = trace.New(os.Stdout)
 	http.Handle("/chat", MustAuth(&templateHandler{filename: "chat.html"}))
