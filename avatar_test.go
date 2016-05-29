6779f2fbe3f14f5c33d2c8dea1ab37acf6f1769c
Ryu Hanaoka <doradora56@gmail.com>
2016-05-29 12:47:56 +0900

    3.2.1 認証サービスを使ったAvatarの実装 p65

---
+++ avatar_test.go
@@ -0,0 +1,23 @@
+package main
+
+import "testing"
+
+func TestAuthAvatar(t *testing.T) {
+	var authAvatar AuthAvatar
+	client := new(client)
+	url, err := authAvatar.GetAvatarURL(client)
+	if err != ErrNoAvatarURL {
+		t.Error("値が存在しない場合, AuthAvatar.GetAvatarURLは ErrorNotAvatarURLを使うべきです。")
+	}
+	// 値をセットします
+	testUrl := "http://url-to-avatar/"
+	client.userData = map[string]interface{}{"avater_ur;": testUrl}
+	url, err = authAvatar.GetAvatarURL(client)
+	if err != nil {
+		t.Error("値が存在する場合、authAvatar.GetAvatarURLはエラーを返すべきではありません")
+	} else {
+		if url != testUrl {
+			t.Error("AuthAvatar.GetAvatarURLは正しいURLを返すべきです")
+		}
+	}
+}
