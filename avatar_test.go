617c0596ecaa894d91af14e6a44ef13ec4e73299
Ryu Hanaoka <doradora56@gmail.com>
2016-06-10 22:24:26 +0900

    3.2.1.3 Gravaterを使った実装 p69

--- avatar_test.go
+++ avatar_test.go
@@ -21,3 +21,16 @@ func TestAuthAvatar(t *testing.T) {
 		}
 	}
 }
+
+func TestGravatarAvatar(t *testing.T) {
+	var gravatarAvatar GravatarAvatar
+	client := new(client)
+	client.userData = map[string]interface{}{"email": "MyEmailAddress@example.com"}
+	url, err := gravatarAvatar.GetAvatarURL(client)
+	if err != nil {
+		t.Error("GavatarAvatar.GetAvatarURLはエラーを返すべきではありません")
+	}
+	if url != "//www.gravatar.com/avatar/abc" {
+		t.Errorf("GravatarAvitar.GetAvatarURLが%sという誤った値を返しました", url)
+	}
+}
