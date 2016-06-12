3652419974d9d33f0f42e212f6bc3e0b91475e3c
Ryu Hanaoka <doradora56@gmail.com>
2016-06-13 01:12:46 +0900

    3.3.2 アップロードのフォーム p74

--- main.go
+++ main.go
@@ -60,6 +60,7 @@ func main() {
 	//r.tracer = trace.New(os.Stdout)
 	http.Handle("/chat", MustAuth(&templateHandler{filename: "chat.html"}))
 	http.Handle("/login", &templateHandler{filename: "login.html"})
+	http.Handle("/upload", &templateHandler{filename: "upload.html"})
 	http.HandleFunc("/auth/", loginHandler)
 	http.HandleFunc("/logout", func(w http.ResponseWriter, r *http.Request) {
 		http.SetCookie(w, &http.Cookie{
